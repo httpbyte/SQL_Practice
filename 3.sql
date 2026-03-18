33956aa9dc20957ee4bdcc2d678eb72a85d1bb10
Roosevelt Mendieta <15744719+httpbyte@users.noreply.github.com>
2026-03-18 18:09:30 -0400

    challenge #3

---
+++ 3.sql
@@ -0,0 +1,15 @@
+/*
+id	first_name	country	    score
+1	Maria	    Germany 	350
+2	John	    USA	        900
+3	Georg	    UK	        750
+4	Martin	    Germany 	500
+5	Peter	    USA	        0
+*/
+
+-- Challenge: Retrieve all customers and sort the results by the highest score first
+
+-- Solution:
+SELECT *
+FROM customers 
+ORDER BY score DESC;
