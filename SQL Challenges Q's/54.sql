d0e4b051e1e083b1d28d635a5afe4ddbed5e3818
Roosevelt Mendieta <15744719+httpbyte@users.noreply.github.com>
2026-03-19 23:58:20 -0400

    added challenge

---
+++ SQL Challenges Q's/54.sql
@@ -0,0 +1,25 @@
+
+/*
+Challenge: 
+Using the salesDB, retrieve a list of all orders, along with the related customer, product, and employee details.
+
+For each order, display: Order ID, Customer's Name, Product name, Sales, Price, Sales person's name 
+*/
+
+-- Solution:
+SELECT 
+o.OrderID,
+o.Sales,
+c.FirstName,
+c.LastName,
+p.Product AS ProductName,
+p.Price,
+e.FirstName AS EmployeeFirstName,
+e.LastName AS EmployeeLastName
+FROM Sales.Orders AS o
+LEFT JOIN Sales.Customers AS c
+ON o.CustomerID = c.CustomerID
+LEFT JOIN Sales.Products AS p
+ON o.ProductID = p.ProductID
+LEFT JOIN Sales.Employees AS e
+ON o.SalesPersonID = e.EmployeeID
